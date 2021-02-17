04bd501e9f96fc54ab2b1bae41e5a97cc907e935
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-02-16 19:56:53 -0800

    first commit, made new repo, finished project

---
+++ index.js
@@ -0,0 +1,62 @@
+function generate_random(max_number) {
+    return Math.round(Math.random()*max_number);
+}
+let button = document.querySelector("button");
+let answer = document.querySelector("#answer");
+
+button.addEventListener("click", function(){
+
+    let randomNumber = generate_random(10);
+    let answerText = "";
+
+    if (randomNumber == 0) {
+        answerText = "Oh Absolutely!";
+
+    }
+    if (randomNumber == 1) {
+        answerText = "Sorry I dont Think So =(";
+
+    }
+    if (randomNumber == 2) {
+        answerText = "Say What Now?";
+
+    }
+    if (randomNumber == 3) {
+        answerText = "No way Dude.";
+
+    }
+    if (randomNumber == 4) {
+        answerText = "Better Ask Vinson..";
+
+    }
+    if (randomNumber == 5) {
+        answerText = "Seriously, Ask Vinson!";
+
+    }
+    if (randomNumber == 6) {
+        answerText = "Sure why not";
+
+    }
+    if (randomNumber == 7) {
+        answerText = "Your Answer might hurt..";
+
+    }
+    if (randomNumber == 8) {
+        answerText = "NOPE!";
+
+    }
+    if (randomNumber == 9) {
+        answerText = "Ask Shmeric";
+
+    }
+    if (randomNumber == 10) {
+        answerText = "without a doubt!";
+
+    }
+
+   
+
+    answer.innerHTML = answerText;
+    
+    
+});
